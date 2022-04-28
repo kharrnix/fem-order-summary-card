97c5cb8284371af46b76e29617fd43e0b17a88e2
Basil <93736879+kharrnix@users.noreply.github.com>
2022-04-28 13:09:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Frontend Mentor - Order summary card solution
+
+This is a solution to the [Order summary card challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/order-summary-component-QlPmajDUj).
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See hover states for interactive elements
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Solution URL: [](#)
+- Live Site URL: [](#)
+
+## My process
+
+### Built with
+
+- Flexbox
+- Mobile-first workflow
+
+### What I learned
+
+The `clamp()` function in CSS is really useful for making things responsive; I used it on the card to make sure it didn't get too big on larger screens.
+
+### Useful resources
+
+- [W3Schools](https://w3schools.com) - Great website for HTML, CSS and
+    JavaScript references
+
+## Author
+
+- Frontend Mentor - [@kharrnix](https://www.frontendmentor.io/profile/kharrnix)
+
---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+const { src, dest, series, watch, task } = require("gulp"),
+  browserSync = require("browser-sync"),
+  sourcemaps = require("gulp-sourcemaps"),
+  postcss = require("gulp-postcss"),
+  cssnano = require("cssnano"),
+  autoprefixer = require("autoprefixer"),
+  terser = require("gulp-terser");
+
+// File paths
+const html = {
+  src: "src/*.html",
+  dist: "./",
+};
+const css = {
+  src: "src/css/**/*.css",
+  dist: "./css",
+};
+const js = {
+  src: "src/js/**/*.js",
+  dist: "./js",
+};
+
+// HTML task
+htmlTask = () => {
+  return src(html.src).pipe(dest(html.dist));
+};
+
+// CSS task
+cssTask = () => {
+  return src(css.src)
+    .pipe(sourcemaps.init())
+    .pipe(postcss([autoprefixer(), cssnano()])) // Postcss and plugins
+    .pipe(sourcemaps.write())
+    .pipe(dest(css.dist));
+};
+
+// JavaScript Task
+javascriptTask = () => {
+  return src(js.src)
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write())
+    .pipe(dest(js.dist));
+};
+
+// Browser sync serve
+browserSyncServe = (callback) => {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+    },
+  });
+  callback();
+};
+// Browser sync reload
+browserSyncReload = (callback) => {
+  browserSync.reload();
+  callback();
+};
+// Browser sync watch
+browserSyncWatch = () => {
+  watch(
+    [css.src, js.src, html.src],
+    series(cssTask, javascriptTask, htmlTask, browserSyncReload)
+  );
+};
+
+}
+
+exports.default = series(
+  htmlTask,
+  cssTask,
+  javascriptTask,
+  browserSyncServe,
+  browserSyncWatch
+);
+
